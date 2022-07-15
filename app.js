6e4815bd2f1227f7e42d7be5e39dad0edc812229
Sophia G20 Grzeskowiak-Amezquita <SophiaG20@SophiaG20.com>
2022-07-15 09:08:09 -0700

    WOw I need to nap

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 'use strict';
+ let siteVisitor = prompt('What is your name?');
+ alert('Welcome ' + siteVisitor)! Think about the temperature where you are;
 
-function getUser(id) {
-  let firstName = prompt('What is your first name?');
-    let lastName = prompt('What is your last name?');
-    let age = prompt('How old are you?');
-    let city = prompt('What city do you live in?');
-    if (id === 1) {
-      return {
-        firstName: firstName,
-        lastName: lastName,
-        age: age,
-        city: city,
-        id: id
-      };
-    }
-  };
+ // LETTSSS GOOOO
+
+ let score = 0;
+
+ function temperature() {
+   let temperature = prompt('What is the temperature outside?');
+   if (temperature > 70) {
+     alert('It is too hot outside.');
+     //console.log('It is too hot outside.');
+   } else if (temperature < 70) {
+     alert('It is too cold outside.');
+     //console.log('It is too cold outside.');
+   } else {
+     alert('It is just right outside.');
+     //console.log('It is just right outside.');
+   }
+ }
